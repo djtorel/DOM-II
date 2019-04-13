791d1bf7a6a59696e6de915d34c55b19d2567eb6
Dominic Torruellas <dom@domtorr.com>
2019-04-13 10:44:00 -0700

    added blur effects to images

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+const bySelector = (selector, cb = false) => {
+  const node = document.querySelector(selector);
+  return cb ? cb(node) : node;
+};
+
+const bySelectorAll = (selector, cb = false) => {
+  const nodeList = document.querySelectorAll(selector);
+  return cb ? cb(nodeList) : nodeList;
+};
+
+const mapAll = (selector, cb) => {
+  const nodeArr = Array.from(bySelectorAll(selector));
+  return nodeArr.map(cb);
+};
+
+mapAll('img', element => {
+  element.addEventListener('mouseenter', e => {
+    e.target.classList.add('blur-image');
+  });
+  element.addEventListener('mouseleave', e => {
+    e.target.classList.remove('blur-image');
+  });
+});
